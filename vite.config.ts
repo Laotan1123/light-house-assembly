9a6f9fd0cff5bc6f35b18358f18f8adcbbfe1cce
Laotan1123 <Laotan2300@gmail.com>
2025-02-22 05:04:11 +0100

    Configured GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,4 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   base: '/light-house-assembly/',
-  optimizeDeps: {
-    exclude: ['lucide-react']
-  },
 });
